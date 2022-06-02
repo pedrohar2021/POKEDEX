2f90f6622c861f3324af5aef8372f523bd96f7e2
Pedro Rezende <pedrohar2021@gmail.com>
2022-06-01 23:06:49 -0300

    Update Project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -85,4 +86,4 @@ app.get("/delete/:id", (req,res) => {
 })
 
 
-app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
+app.listen(port, () => console.log('Servidor rodando em http://localhost:3000'));
